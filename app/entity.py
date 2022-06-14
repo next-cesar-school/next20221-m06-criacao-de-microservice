9dc4cb5defcf63d7139da838576bb681bae8c171
Pedro Neves <pedronevesbezerra@gmail.com>
2022-06-14 15:37:27 -0300

    adiciona comentario

--- app/entity.py
+++ app/entity.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from app.config_db import db
 from sqlalchemy import ForeignKey
 
@@ -69,7 +70,7 @@ class UsersEntity(db.Model):
 	data_nascimento = db.Column(db.String(10))
 	cargo = db.Column(db.String(10))
 	matricula = db.Column(db.String(50), unique=True)
-	status = db.Column(db.String(50))
+	status = db.Column(db.String(50), Enum('ativo', 'inativo'))
 	id_centro_custo = db.Column(db.Integer, ForeignKey('cost_center.id'))
 
 	gerente = db.relationship (ProjectEntity)
